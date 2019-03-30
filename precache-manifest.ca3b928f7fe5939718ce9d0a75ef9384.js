2c487ba88dc21a2df749148c4f458ea9ce19fb2e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-30 08:46:51 +0000

    Deploy sjfkai/hello-pages to github.com/sjfkai/hello-pages.git:gh-pages

---
+++ precache-manifest.ca3b928f7fe5939718ce9d0a75ef9384.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "40fe4d304d04c9f424b4",
+    "url": "/static/js/main.ec9750d9.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "40fe4d304d04c9f424b4",
+    "url": "/static/css/main.9150dafd.chunk.css"
+  },
+  {
+    "revision": "5c0a9c55fb65f82602cd5063d5fb8ab5",
+    "url": "/index.html"
+  }
+];
