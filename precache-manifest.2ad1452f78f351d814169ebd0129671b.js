f4bdf47f92860343258e4a012baf06a4864a2a0a
sjfkai <sjfkai@163.com>
2019-03-30 14:57:49 +0800

    Updates

---
+++ precache-manifest.2ad1452f78f351d814169ebd0129671b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "047aea05e09f61486ecc",
+    "url": "/static/js/main.30c4c7c7.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "047aea05e09f61486ecc",
+    "url": "/static/css/main.4ef1c57d.chunk.css"
+  },
+  {
+    "revision": "9aa1d647aa56d6f3b30fc5db6b15c321",
+    "url": "/index.html"
+  }
+];
